8f1c4937f3d77001d6aedc0a415e438743252164
Jerin <jerin.kuriakose@dreamorbit.com>
2021-12-03 13:41:19 +0530

    new changes

--- src/app/meenal-demo/meenal-demo.component.ts
+++ src/app/meenal-demo/meenal-demo.component.ts
@@ -8,36 +8,36 @@ import { MeenalTasksService } from '../meenal-tasks.service';
 })
 export class MeenalDemoComponent implements OnInit {
   commonId: number = 1400
-  letStet1to3:any[] = []
-  step4to7:any[]= []
-  finalObj ={}
+  letStep1to7: any[] = []
+  ImageList:any["."] | undefined
   constructor(private _services: MeenalTasksService) { }
   ngOnInit(): void {
     this.finalfunctions()
-    console.log(this.letStet1to3)
-    console.log(this.step4to7)
+    console.log(this.letStep1to7)
   }
 
 
   finalfunctions() {
-    this._services.getCompleted(this.commonId).subscribe((resc:any)=>{
-      resc.data.map((item:any)=>{
+    this._services.getCompleted(this.commonId).subscribe((resc: any) => {
+      resc.data.map((item: any) => {
         let payload = {
-          ReturnStatusDisplay:item[13],
-          ReturnStatusDTM:item[14]
+          ReturnStatusDisplayC: item[13],
+          ReturnStatusDTM: item[14],
+          ReturnStatusDisplayF: "",
+          
         }
-        this.letStet1to3.push(payload)
-      
+        this.letStep1to7.push(payload)
       })
-
     })
 
-    this._services.getFuture(this.commonId).subscribe((resf:any)=>{
-      resf.data.map((item2:any)=>{
-        let payload4t7={
-          ReturnStatusDisplay:item2[2]
+    this._services.getFuture(this.commonId).subscribe((resf: any) => {
+      resf.data.map((item2: any) => {
+        let payload = {
+          ReturnStatusDisplayC: "",
+          ReturnStatusDTM: "",
+          ReturnStatusDisplayF: item2[2]
         }
-        this.step4to7.push(payload4t7)
+        this.letStep1to7.push(payload)
       })
     })
 
